9be0aa3b78630dceee8a364963a73868af559151
lapkot <byyy.com@gmail.com>
2022-02-24 20:32:17 +0100

    feat: add implementation for check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  let brackets = bracketsConfig
+    .reduce((acc, brackets) => ({
+      ...acc, [brackets[0]]: brackets[1],
+    }), {});
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    const symbol = str[i];
+    if (
+        (symbol !== brackets[symbol] || arr[arr.length - 1] !== symbol)
+        && Object.keys(brackets).includes(symbol)
+      ) {
+      arr.push(symbol);
+    } else {
+      const lastSymbol = arr.pop();
+      if (brackets[lastSymbol] !== symbol) {
+        return false;
+      }
+    }
+  };
+  return arr.length === 0
 }
+
+module.exports = check;
